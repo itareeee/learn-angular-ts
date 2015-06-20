6f38ffbd87f3d7430da88f50af8b68df7df66fb4
Itaru Kitagawa <itaruxkita@gmail.com>
2015-06-20 22:43:57 +0900

    setting up

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var webserver = require('gulp-webserver');
 
-gulp.task('default', function() {
-  console.log('ababa!');
+
+gulp.task("default", function() {
+
+  gutil.log(gutil.colors.yellow("=============================="));
+  gutil.log(gutil.colors.cyan(" L E T ' S   L E A R N"));
+  gutil.log(gutil.colors.cyan(" A N G U L A R - T S !!"));
+  gutil.log(gutil.colors.yellow("=============================="));
+
 });
+
+
+gulp.task("webserver", function(){
+  return gulp.src('app')
+    .pipe(webserver());
+})
