889906e9806f0c48ad5089ca0bf66bbf31d1ed94
kyouya.baba <kyouya.baba@bizreach.co.jp>
2015-07-01 20:20:49 +0900

    baba-homework0710

---
+++ app/baba/homework0710/app.ts
@@ -0,0 +1,10 @@
+angular.module('invoiceApp', ['ng']).controller('invoiceController', function() {
+
+  this.quantity = 1;
+  this.costs = 2;
+  this.pay = function() {
+    alert('thanks!');
+    return false;
+  };
+
+});
