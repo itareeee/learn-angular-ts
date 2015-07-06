1e4d7e3d2c60f4bdb9fb023242f22ebda004c5a0
omiend <omiend@gmail.com>
2015-07-06 14:05:13 +0900

    homework day2 ちょっと修正

--- app/omiend/sample02/homework.js
+++ app/omiend/sample02/homework.js
@@ -16,4 +16,4 @@ var Sample02HomeworkFilterController = (function () {
     };
     return Sample02HomeworkFilterController;
 })();
-angular.module('sample02HomeworkApp', []).controller('FilterController', Sample02HomeworkFilterController);
+angular.module('sample02HomeworkApp', ['ng']).controller('FilterController', Sample02HomeworkFilterController);
