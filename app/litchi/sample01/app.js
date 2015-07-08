aeeb00841549d47dcb8766b68cb05316d75a0e28
candyrobot <hideaki.tanaka@bizreach.co.jp>
2015-07-08 15:01:50 +0900

    add litchi

---
+++ app/litchi/sample01/app.js
@@ -0,0 +1,11 @@
+// var Foo = require('./foo');
+
+// var foo = new Foo('sato');
+
+// var out = foo.method(); // -> 'sato'
+// console.log(out);
+
+
+var utils = require('./utils');
+console.log(utils.string.nl2br());
+
---
+++ app/litchi/sample01/utils.js
@@ -0,0 +1,22 @@
+// module.exports = (function() {
+//   function Foo(name) {
+//     this.name = name;
+//   }
+
+//   Foo.prototype.method = function method() {
+//     return this.name;
+//   };
+
+//   return Foo;
+// })();
+
+
+module.exports = (function(){
+	return {
+		string : {
+			nl2br : function(){
+				return 1;
+			}
+		},
+	}
+})();
