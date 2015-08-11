741c98c3fb12aca1f7a0c526d93a6e9fbf7893d2
omiend <omiend@gmail.com>
2015-08-11 10:39:05 +0900

    day11まで

---
+++ app/omiend/sample04/app.ts
@@ -0,0 +1,35 @@
+///<reference path="../../../typings/tsd.d.ts" />
+angular.module('sampleApp', [])
+  .directive('comboBox', function(){
+      return {
+        scope: {
+          selectedItem: '=',
+          allItems: '='
+        },
+        restrict: 'EA',
+        template: '<div class="combobox">' +
+                  '<input type="text" ng-model="selectedItem">' +
+                  '  <ul ng-show="isFocus">' +
+                  '   <li ng-repeat="item in allItems" ng-click="click($event, item)">'+
+                  '     {{item}}'+
+                  '   </li>' +
+                  '  </ul>' +
+                  '</div>',
+        link: function(scope: any, iElement) {
+          scope.isFocus = false;
+
+          iElement.find('input')
+            .on('focus', function(){
+                scope.$apply(function(){
+                  scope.isFocus = true;
+                });
+              });
+          scope.click = function($event, item) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            scope.selectedItem = item;
+            scope.isFocus = false;
+          };
+        }
+      }
+    });
