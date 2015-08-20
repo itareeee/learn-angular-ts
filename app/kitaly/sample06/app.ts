148225900ceb2e01ea0bf52c4bcb63673c79142b
Itaru Kitagawa <itaruxkita@gmail.com>
2015-08-20 10:34:14 +0900

    little adjust

--- app/kitaly/sample06/app.ts
+++ app/kitaly/sample06/app.ts
@@ -7,30 +7,30 @@ module kitaly.sample06x {
     angular
       .module('sample06App', ['ng'])
       .controller('tabSetXController', function($scope){
-	this.tabs = $scope.tabs = [];
-	this.addTab = function(tab) {
-	  this.tabs.push(tab);
-	}
-      })
-    .directive('tabSetX', function(){
-      return {
-	restrict: 'E',
-	scope: {},
-	template: 
-	  '<select ng-model="selectedTab" ng-options="tab.header for tab in tabs"> ' +
-	  '</select>' + 
-	  '<div ng-transclude></div>',
-	controller: 'tabSetXController',
-	transclude: true,
-	link: function(scope: any, element, attrs, tabSetCtrl) {
-	  scope.$watch('selectedTab', function(selectedTab){
-	    if(selectedTab){
-	      angular.forEach(scope.tabs, function(tab){
-		tab.isActive = tab.header == selectedTab.header;
-	      })
-	    }
-	  });
-	}
+				this.tabs = $scope.tabs = [];
+				this.addTab = function(tab) {
+					this.tabs.push(tab);
+				}
+						})
+					.directive('tabSetX', function(){
+						return {
+				restrict: 'E',
+				scope: {},
+				template:
+					'<select ng-model="selectedTab" ng-options="tab.header for tab in tabs"> ' +
+					'</select>' +
+					'<div ng-transclude></div>',
+				controller: 'tabSetXController',
+				transclude: true,
+				link: function(scope: any, element, attrs, tabSetCtrl) {
+					scope.$watch('selectedTab', function(selectedTab){
+						if(selectedTab){
+							angular.forEach(scope.tabs, function(tab: any){
+								tab.isActive = tab.header == selectedTab.header;
+							})
+						}
+					});
+				}
       }
     })
     .directive('tabX', function(){
