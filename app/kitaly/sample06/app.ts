b9f55cf0f9f5a4da2197ae84071b7187dd32d41a
森田 賢二 <kenji.morita@bizreach.co.jp>
2015-08-26 10:42:26 +0900

    directive

--- app/kitaly/sample06/app.ts
+++ app/kitaly/sample06/app.ts
@@ -1,38 +1,38 @@
 ///<reference path="../../../typings/tsd.d.ts" />
 
-module kitaly.sample06x {
+module morita.sample06x {
 
   export function init(){
 
     angular
       .module('sample06App', ['ng'])
       .controller('tabSetXController', function($scope){
-	this.tabs = $scope.tabs = [];
-	this.addTab = function(tab) {
-	  this.tabs.push(tab);
-	}
-      })
+    	this.tabs = $scope.tabs = [];
+    	this.addTab = function(tab) {
+      	this.tabs.push(tab);
+    	}
+    })
     .directive('tabSetX', function(){
       return {
-	restrict: 'E',
-	scope: {},
-	template: 
-	  '<select ng-model="selectedTab" ng-options="tab.header for tab in tabs"> ' +
-	  '</select>' + 
-	  '<div ng-transclude></div>',
-	controller: 'tabSetXController',
-	transclude: true,
-	link: function(scope: any, element, attrs, tabSetCtrl) {
-	  scope.$watch('selectedTab', function(selectedTab){
-	    if(selectedTab){
-	      angular.forEach(scope.tabs, function(tab){
-		tab.isActive = tab.header == selectedTab.header;
+      	restrict: 'E',
+      	scope: {},
+      	template:
+      	  '<select ng-model="selectedTab" ng-options="tab.header for tab in tabs"> ' +
+      	  '</select>' +
+      	  '<div ng-transclude></div>',
+      	controller: 'tabSetXController',
+      	transclude: true,
+      	link: function(scope: any) {
+      	  scope.$watch('selectedTab', function(selectedTab){
+      	    if(selectedTab){
+      	      angular.forEach(scope.tabs, function(tab){
+      		tab.isActive = tab.header == selectedTab.header;
 	      })
 	    }
 	  });
 	}
-      }
-    })
+}
+})
     .directive('tabX', function(){
       return {
 	restrict: 'E',
@@ -46,18 +46,64 @@ module kitaly.sample06x {
 	}
       }
     })
-  
+
   }
 
 }
 
-module kitaly.sample06 {
+module morita.sample06 {
+  export interface fafa {
+    restrict : string;
+    template : string;
+    scope: any;
+    transclude: boolean;
+    controller:  string;
+    link:(scope:ng.IScope,element:ng.IAugumentedJQuery,attr:ng.IAttributes)=>{}: void
+  }
+  export class TabSetDirective implements ng.IDirective{
+    public selectedTab: any;
+    public _this: any;
+    consutructor(){
+      _this = this.scope;
+      this.init()
+    }
+    Factory() = ()=>{
+      var TabSetDirective = ()=>{
+        return new TabSetDirective()
+      }
+    }
+    init() =　()=>{
+      this.restrict = 'E';
+      this.tamplate = '<select ng-model="selectedTab" ng-options="tab.header for tab in tabs"> ' +
+            	           '</select>' +
+            	        '<div ng-transclude></div>',
+      this.controller = 'tabSetXController';
+      this.transclude = true;
+      this.link = (_this: any, element, attrs, tabSetCtrl) {
+      	             _this.header = attrs.header;
+      	              tabSetCtrl.addTab(_this);
+      	        }
 
+        }
+      }
+    }
+  }
 
-  class TabSetDirective implements ng.IDirective{}
-  class TabSetDController {}
+export interface fafa {
+  scope: ng.IDirective;
+}
+export class TabSetDController implements fafa {
+  tab: any;
+  constructor(public tabs){
+  this.tabs = tabs[];
+  this.addTab();
+  }
+  addTab = ()=>{
+    return this.tabs.push(this.tab);
+  }
+}
   class TabDirective {}
-  
+
   export function init(){
     //Init Process
   }
